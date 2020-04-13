0acd6099f755195f46c1776ca54140a12fc7eebb
André Marques <andre.b.marques@gmail.com>
2020-04-13 17:15:38 +0100

    Create SearchF5.sh

---
+++ SearchF5.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "##$(tput setaf 1)$(tput setab 7)F5 - LB - Description$(tput sgr 0)##"
+output=$(curl -sku 'user:password' -X POST -H "Content-type: application/json" -d "{\"command\":\"run\", \"utilCmdArgs\": \"-c 'tmsh list ltm virtual one-line | grep $1'\"}" "https://URL/mgmt/tm/util/bash" | jq -r ".commandResult";)
+echo "$output"
+echo "$(tput setaf 1)$(tput setab 7)##Pool##$(tput sgr 0)"
+var1=$(grep -oP '(?<=pool ).*?(?= profiles)' <<< "$output";)
+echo "$var1"
+output2=$(curl -sku 'user:password' -X POST -H "Content-type: application/json" -d "{\"command\":\"run\", \"utilCmdArgs\": \"-c 'tmsh list ltm pool one-line | grep '$var1' '\"}" "https://URL/mgmt/tm/util/bash" | jq -r ".commandResult";)
+echo "$(tput setaf 1)$(tput setab 7)##Members##$(tput sgr 0)"
+echo "$output2"
